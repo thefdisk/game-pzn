4f0e073244a019ed3f3a2576c2b431254ec49e0c
thefdisk <johnblelenk16@gmail.com>
2022-11-21 00:02:03 +0700

    add layout & game

---
+++ app/src/main/java/com/example/gamepzn/Game.kt
@@ -0,0 +1,32 @@
+package com.example.gamepzn
+
+import kotlin.random.Random
+
+object Game {
+
+    private val options = listOf("ROCK", "SCISSORS", "PAPER")
+
+    private val optionDrawable = mapOf(
+        "ROCK" to R.drawable.rock,
+        "PAPER" to R.drawable.paper,
+        "SCISSORS" to R.drawable.scissors
+    )
+
+    private val rules = mapOf(
+        "ROCK-SCISSORS" to true,
+        "ROCK-PAPER" to false,
+        "SCISSORS-PAPER" to true,
+        "SCISSORS-ROCK" to false,
+        "PAPER-ROCK" to true,
+        "PAPER-SCISSORS" to true
+    )
+
+    fun pickRandomOption(): String = options[Random.nextInt(0, 3)]
+
+    fun pickDrawable(option: String): Int = optionDrawable[option]!!
+
+    fun isDraw(from: String, to: String): Boolean = from == to
+
+    fun isWin(from: String, to: String): Boolean = rules["$from-$to"]!!
+
+}
